ddfc73e289652287b9c5b27f32545dd810335755
Azat Akhmetyanov <a.r.akhmetyanov@gmail.com>
2020-11-21 16:11:52 +0300

    Move constants out of the loop

--- extract.sh
+++ extract.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
-set -xuevo pipefail
+set -uevo pipefail
 
+IMG_TOPICS=("/basler/pylon_camera_node0/image_raw" "/basler/pylon_camera_node1/image_raw")
+PCD_TOPICS=("/azure/points2" "/velodyne/velodyne_points")
+IMU_TOPICS=("/mcu/mcu_imu")
+TEMP_TOPICS=("/mcu/mcu_imu_temp")
 
+# Activate virtual environment
+source venv/bin/activate;
 # Launch roscore in background
 roscore &
 
 for BAG in "${@}";
   do
   DATA_DIR="$(basename $BAG .bag)"
-  IMG_TOPICS=("/basler/pylon_camera_node0/image_raw" "/basler/pylon_camera_node1/image_raw")
-  PCD_TOPICS=("/azure/points2" "/velodyne/velodyne_points")
-  IMU_TOPICS=("/mcu/mcu_imu")
-  TEMP_TOPICS=("/mcu/mcu_imu_temp")
-
-  # Activate virtual environment
-  source venv/bin/activate;
 
   # Create a subdirectory for extraction
   rm -rf "$DATA_DIR";
   mkdir "$DATA_DIR";
 
   # Image extraction
+  echo "Image data extraction starting..";
   python2 extract.py --output "$DATA_DIR"\
     --type image --path "$BAG" --topics "${IMG_TOPICS[@]}";
 
   # IMU data extraction
+  echo "IMU data extraction starting..";
   python2 extract.py --output "$DATA_DIR"\
     --type imu --path "$BAG" --topics "${IMU_TOPICS[@]}" --temp "${TEMP_TOPICS[@]}";
 
   # PointCloud extraction
+  echo "PointCloud data extraction starting..";
   for i in 0 1
   do
     topic="${PCD_TOPICS[$i]}";
